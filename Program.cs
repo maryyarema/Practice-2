13fbd4c66297866d8c7d4174bf823b5529e427c3
maryyarema <113243021+maryyarema@users.noreply.github.com>
2022-09-26 01:04:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter n");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i < n; i++)
+        {
+            if ( (i % 5 != 0)  && (i % 3 != 0) && (i % 7 != 0)&&(i % 2 != 0) || i == 2 || i == 3 || i == 5 || i == 7)
+
+                Console.Write($"{i} ");
+        }
+
+    }
+}
